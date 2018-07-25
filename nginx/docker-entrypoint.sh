716f03119b9de6b9fcdf86cb9fce2d8d742465a6
Emanuele Disco <emawind84@gmail.com>
2018-07-25 15:08:23 +0900

    first commit

---
+++ nginx/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+export SSL=${SSL_ENABLED:?missing}
+
+echo "Starting HTTP server with the following settings:"
+echo "- ssl enabled:     ${SSL_ENABLED}"
+
+if [ "$SSL_ENABLED" == "1" ] && [ -f '/usr/share/nginx/conf.d/ssl.conf.bak' ]; then
+    mv /usr/share/nginx/conf.d/ssl.conf.bak /usr/share/nginx/conf.d/ssl.conf
+    sed -i 's/listen 80/listen 443 ssl http2/' /usr/share/nginx/conf.d/pmis.conf
+fi
+
+if [ "$NAMESERVERS" == "" ]; then
+    export NAMESERVERS=$(cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' ')
+fi
+
+envsubst '$NAMESERVERS' < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
+
+exec nginx -g "daemon off;" "$@"
